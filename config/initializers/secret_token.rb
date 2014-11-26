c4de86ac75197ad3d0e0b6eef41c2c55f3985145
Uriel <Uriel@joses-iMac.local>
2014-11-25 20:26:05 -0600

    Upload Project ActiveAdmin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveAdmin::Application.config.secret_token = 'a2a47e83826422aba77726ae8740846ccd5d45c3f35ec60e210dd7f27f849cf0039719749d1f5322414feaaa0fb883b0762cd0f818706f46729c3bd262d63a8b'
